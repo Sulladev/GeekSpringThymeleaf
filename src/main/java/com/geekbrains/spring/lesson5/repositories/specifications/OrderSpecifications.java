79d320c02c2266df1e463f6103ad457af57fa15c
sulla <inikolazzz@gmail.com>
2020-12-03 16:21:31 +0300

    specification is ok

--- src/main/java/com/geekbrains/spring/lesson5/repositories/specifications/OrderSpecifications.java
+++ src/main/java/com/geekbrains/spring/lesson5/repositories/specifications/OrderSpecifications.java
@@ -6,14 +6,14 @@ import org.springframework.data.jpa.domain.Specification;
 public class OrderSpecifications {
 
     public static Specification<Order> priceGreaterOrEqualsThan(Integer minPrice) {
-        return (Specification<Order>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("current_Price"), minPrice);  // where p.price >= minPrice
+        return (Specification<Order>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("currentPrice"), minPrice);  // where p.price >= minPrice
     }
 
     public static Specification<Order> priceLesserOrEqualsThan(Integer maxPrice) {
-        return (Specification<Order>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("current_Price"), maxPrice); // where p.price <= maxPrice
+        return (Specification<Order>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("currentPrice"), maxPrice); // where p.price <= maxPrice
     }
 
     public static Specification<Order> titleLike(String titlePart) {
-        return (Specification<Order>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("order"), String.format("%%%s%%", titlePart)); // where p.title like %titlePart%
+        return (Specification<Order>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("code"), String.format("%%%s%%", titlePart)); // where p.title like %titlePart%
     }
 }
